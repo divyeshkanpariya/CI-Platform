982f91e1f0a579c0c5b6befdd3c6060d535c8a7f
Divyesh <divyeshkanpariya3626@gmail.com>
2023-03-08 13:44:03 +0530

    new

--- MVC/CI-Platform/CI-Platform/Program.cs
+++ MVC/CI-Platform/CI-Platform/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddScoped<IResetPasswordRepository, ResetPasswordRepository>();
 
 
 builder.Services.AddScoped<IRegistrationRepository, RegistrationRepository>();
+
 builder.Services.AddSession();
 builder.Services.AddMemoryCache();
 var app = builder.Build();
