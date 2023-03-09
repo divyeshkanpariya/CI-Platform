07ccb18413f7bed23094a2e5d3d2c7cd4c8d9f06
Divyesh <divyeshkanpariya3626@gmail.com>
2023-03-10 00:43:24 +0530

    commit wfh

---
+++ MVC/CI-Platform/Models/Models/MissionSeat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CI_Platform.Models.Models;
+
+public partial class MissionSeat
+{
+    public long MissionSeatId { get; set; }
+
+    public long MissionId { get; set; }
+
+    public byte Islimited { get; set; }
+
+    public int? TotalSeats { get; set; }
+
+    public int? SeatsFilled { get; set; }
+
+    public DateTime CreatedTime { get; set; }
+
+    public DateTime? UpdatedTime { get; set; }
+
+    public DateTime? DeletedTime { get; set; }
+
+    public virtual Mission Mission { get; set; } = null!;
+}
