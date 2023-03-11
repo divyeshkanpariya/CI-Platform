44697f7a65dfcf4dd5f19a158558e4576fc7a11b
Divyesh <divyeshkanpariya3626@gmail.com>
2023-03-11 16:56:36 +0530

    11/03/2023 mid

--- MVC/CI-Platform/CI-Platform/wwwroot/js/site.js
+++ MVC/CI-Platform/CI-Platform/wwwroot/js/site.js
@@ -193,3 +193,14 @@ $('input[type=checkbox]').on("click", function () {
 */
 
 /*$('#selectedFilters').appendChild()*/
+function clearAll() {
+    console.log("clicked");
+    $('input[type=checkbox]:checked').each(function () {
+        console.log($(this));
+        //$(this).checked = false;
+        $(this).prop('checked', false);
+        $('#selectedFilters').empty();
+
+
+    });
+}
